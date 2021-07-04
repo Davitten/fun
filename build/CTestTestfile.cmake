0e15a796b4ff1ea85f388bfe09efc6f2022ca7a0
Martin Davidsen <mada@mir-robots.com>
2021-07-04 09:43:46 +0200

    created map pkg

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/martin/fun/src
+# Build directory: /home/martin/fun/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("navigation/map")
---
+++ build/navigation/map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/martin/fun/src/navigation/map
+# Build directory: /home/martin/fun/build/navigation/map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
